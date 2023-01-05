129936af5f7239e91dfd0780c014cb5b6ec1cafc
Alejandro Ávila <47830091+JMA-Dv@users.noreply.github.com>
2023-01-04 21:34:12 -0600

    Revert "F pagination"

--- src/Core.DTOs/Identity/ApplicationRoleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Core.DTOs.Identity
-{
-    public class ApplicationRoleDto
-    {
-        public List<ApplicationUserRoleDto> UserRoles { get; set; }
-    }
-}
--- src/SPA.Client/Startup.cs
+++ src/SPA.Client/Startup.cs
@@ -16,7 +16,6 @@ namespace SPA.Client
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,17 +27,14 @@ namespace SPA.Client
             }
 
             app.UseRouting();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
-
-            app.UseSpa(spa =>
-            {
-                spa.UseProxyToSpaDevelopmentServer("http://localhost:8080");
-            });
-
-            
         }
     }
 }
